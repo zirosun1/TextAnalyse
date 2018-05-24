fde7731d7746ce117a93ec84f41e69ce8e35f718
zirosun1 <haneange@gmail.com>
2018-05-24 06:47:17 +0300

    2

--- Analizator.cs
+++ Analizator.cs
@@ -10,10 +10,36 @@ namespace TextAnalyse
     {
         public static void AnaliseText(string text)
         {
-            var words = from str in text.Split(new Char[] { ' ', ',', '.', ':', '\t' })
-                                 orderby str
-                                 select str;
-            foreach (var i in words) Console.WriteLine(i);
+            var punctuation = text.Where(Char.IsPunctuation).Distinct().ToArray();
+            var words = text.Split(' ').Select(x => x.Trim(punctuation));
+            List<PairsWordsStrNum> pairs = new List<PairsWordsStrNum>();
+            int num = 1;
+            foreach (var i in words)
+            {
+                if (i.Contains("\n")) num++;
+                pairs.Add(new PairsWordsStrNum(i, num));
+            }
+            List<PairsWordsStrNum> sortedPairs = SortPairs(pairs);
+
+            var countedPars = sortedPairs
+                .GroupBy(x => x.Word)
+                
+                                .Select(g => new { Value = g.Key, Count = g.Count() });
+
+            foreach(var i in countedPars) Console.WriteLine("{0}, {1}", i.Value, i.Count);
+            //foreach (var i in sortedPairs) Console.WriteLine("{0}, {1}", i.Word, i.StrNum);
+        }
+
+        private static List<PairsWordsStrNum> SortPairs(List<PairsWordsStrNum> pairs)
+            {
+            List<PairsWordsStrNum> list = pairs;
+            list.Sort((PairsWordsStrNum x, PairsWordsStrNum y) =>
+            {
+                return x.Word.CompareTo(y.Word);
+            });
+            return list;
+
+
         }
     }
 }
---
+++ PairWordStrNum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalyse
+{
+    class PairsWordsStrNum
+    {
+        public PairsWordsStrNum(string str,int num)
+        {
+            Word = str;
+            StrNum = num;
+        }
+        public string Word { get; set; }
+        public int StrNum { get; set; }
+    }
+}
