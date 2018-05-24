fc93ca5ea7b4a6591c5235879b4331ebb357973c
zirosun1 <haneange@gmail.com>
2018-05-24 04:11:34 +0300

    Initial commit

---
+++ Analizator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalyse
+{
+    public class Analizator
+    {
+        public static void AnaliseText(string text)
+        {
+            var words = from str in text.Split(new Char[] { ' ', ',', '.', ':', '\t' })
+                                 orderby str
+                                 select str;
+            foreach (var i in words) Console.WriteLine(i);
+        }
+    }
+}
